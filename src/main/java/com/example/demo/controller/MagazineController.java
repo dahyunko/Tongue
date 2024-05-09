6b1a6ab70abbddfb1e7b9ec8f62164b24d4052e7
지수영 <jisooyoung97@gmail.com>
2024-05-09 13:24:11 +0900

    feat: first commit

--- src/main/java/com/example/demo/controller/MagazineController.java
+++ src/main/java/com/example/demo/controller/MagazineController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/magazine")
 public class MagazineController {
 }
