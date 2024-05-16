7c4254404732a5c27da0988cb044ab243c356b46
고다현 <kdhgby@naver.com>
2024-05-16 22:07:34 +0900

    login

--- src/main/java/com/example/demo/config/CorsMvcConfig.java
+++ src/main/java/com/example/demo/config/CorsMvcConfig.java
@@ -8,6 +8,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173");
+                .allowedOrigins("http://localhost:5173");
     }
 }
